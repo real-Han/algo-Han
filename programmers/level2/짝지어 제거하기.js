4a30b646167a75733460be59e04151fda3564525
hanSong2 <rlagksd@naver.com>
2020-08-11 20:19:15 +0900

    [prog] 짝지어 제거하기

---
+++ programmers/level2/짝지어 제거하기.js
@@ -0,0 +1,8 @@
+function solution(s) {
+    const stack = [];
+    for (let c of s) {
+        if (stack.length > 0 && stack[stack.length - 1] === c) stack.pop();
+        else stack.push(c);
+    }
+    return stack.length === 0 ? 1 : 0;
+}
