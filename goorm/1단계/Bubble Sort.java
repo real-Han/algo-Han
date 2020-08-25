1051605e0ae9c0eec5883741c9031f5b6353a95c
hanSong2 <rlagksd@naver.com>
2020-08-25 20:52:22 +0900

    [goorm] Bubble Sort

---
+++ goorm/1단계/Bubble Sort.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; ++i) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		for (int i = 0; i < N; ++i) {
+			System.out.print(arr[i] + " ");
+		}
+	}
+}
