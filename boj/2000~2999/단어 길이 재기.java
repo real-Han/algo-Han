39d4050f090c5bfd38e8b42c33f4c1b14052ddd1
han <rlagksd@naver.com>
2020-10-15 23:48:02 +0900

    [boj] 단어 길이 재기

---
+++ boj/2000~2999/단어 길이 재기.java
@@ -0,0 +1,8 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String args[]) throws Exception {
+        System.out.println(new BufferedReader(new InputStreamReader(System.in)).readLine().length());
+    }
+}
